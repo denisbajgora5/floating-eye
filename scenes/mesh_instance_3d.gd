89b865286dd2cbcd4df7399f2f7cca68460de696
JasonOS03 <=>
2026-03-11 16:59:08 +0000

    completed the path drawing functionality.The full path is now being shown in the editor, and the boid follows this path

--- scenes/mesh_instance_3d.gd
+++ scenes/mesh_instance_3d.gd
@@ -10,11 +10,14 @@ func _ready() -> void:
 		var path_curve: Curve3D = path.curve
 		path_points = []
 		
+		self.transform = path.transform
+		var len = path_curve.get_baked_length()
+		
 		for r in range(res):
 			var res_length := float(res-1);
 			var path_res := float(r)/res_length
-			path_points.append(path_curve.sample(path_res,true))
-			
+			var local_path_point := path_curve.sample_baked(path_res * len)
+			path_points.append(local_path_point)
 		var mesh_arrays := []
 		mesh_arrays.resize(Mesh.ARRAY_MAX)
 		mesh_arrays[Mesh.ARRAY_VERTEX] = path_points
@@ -22,6 +25,14 @@ func _ready() -> void:
 		var the_mesh := ArrayMesh.new()
 		the_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINE_STRIP,mesh_arrays
 		)
+		
+		the_mesh.custom_aabb = AABB(Vector3(-1000, -1000, -1000), Vector3(2000, 2000, 2000))
+
+		
+		var mat := StandardMaterial3D.new()
+		mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+		mat.albedo_color = color_of_path
+		self.material_override = mat
 		
 		self.mesh = the_mesh
 		
