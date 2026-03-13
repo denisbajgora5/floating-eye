9d3ece4c63f48024bc2a0b7642d1b1fb9ee851b1
JasonOS03 <=>
2026-03-13 15:36:05 +0000

    added a new node called FollowPath to allow the main boid to follow the path. The script uses parts of the path script from the miniature rotary phone repo

---
+++ scenes/follow_path.gd
@@ -0,0 +1,30 @@
+class_name FollowPath extends SteeringBehavior
+
+var pathIndex = 0
+
+@export var path:Path3D
+@export var waypoint_seek_distance = 3
+
+var target
+
+func on_draw_gizmos():
+	if target:
+		DebugDraw3D.draw_position(Transform3D(Basis(), target), Color.CYAN)
+
+func calculate():
+	var the_point = path.get_curve().get_point_position(pathIndex)
+	target = (path.global_transform * the_point) + boid.vel.normalized() * 2.0
+	var dist = boid.global_transform.origin.distance_to(path.global_transform*the_point)
+	if dist < waypoint_seek_distance:
+		pathIndex = (pathIndex + 1) % path.get_curve().get_point_count()
+	return boid.seek_force(target)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	boid = get_parent()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
